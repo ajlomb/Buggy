1b8b17401073f9680d355a290a342b2b0799c27a
Alexander Lombardo <alexanderjlombardo@gmail.com>
2016-05-02 17:10:42 -0700

    comments stuff out, made a cursoradapter(?)

--- Buggy/app/src/main/java/com/example/alexanderlombardo/project2buggy/BugSQLiteOpenHelper.java
+++ Buggy/app/src/main/java/com/example/alexanderlombardo/project2buggy/BugSQLiteOpenHelper.java
@@ -1,6 +1,8 @@
 package com.example.alexanderlombardo.project2buggy;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -22,7 +24,7 @@ public class BugSQLiteOpenHelper extends SQLiteOpenHelper {
 
     public static final String[] BUG_COLUMNS = {COL_ID, COL_COMMON_NAME, COL_LATIN_NAME, COL_NUM_LEGS, COL_WINGS, COL_COLOR, COL_DESCRIPTION};
 
-    private static final String BUG_TABLE =
+    private static final String CREATE_BUG_TABLE =
             "CREATE TABLE " + BUG_TABLE_TITLE +
                     "(" + COL_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     COL_COMMON_NAME + " TEXT " +
@@ -32,17 +34,39 @@ public class BugSQLiteOpenHelper extends SQLiteOpenHelper {
                     COL_COLOR + " TEXT " +
                     COL_DESCRIPTION + " TEXT";
 
+    public static final String DROP_BUG_TABLE = "DROP TABLE IF EXISTS " + CREATE_BUG_TABLE;
+
     public BugSQLiteOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BUG_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL(DROP_BUG_TABLE);
+        onCreate(db);
     }
+
+    public void insertBugData(int id, String commonName, String latinName, ){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("_id", id);
+        values.put("COMMON_NAME", commonName);
+        values.put("LATIN_NAME", latinName);
+
+        db.insert("BUG_TABLE", null, values);
+    }
+
+    public Cursor getBugs() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor bugsCursor = db.query(DATABASE_NAME, BUG_COLUMNS, null, null, null, null, null, null);
+        return bugsCursor;
+    }
+
 }
