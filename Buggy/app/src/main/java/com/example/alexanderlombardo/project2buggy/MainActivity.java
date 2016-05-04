c9311a298360fa63f754def43d6d0cc2e3d79077
Alexander Lombardo <alexanderjlombardo@gmail.com>
2016-05-04 13:18:41 -0700

    Moves code around, databases that were left open are now closed

--- Buggy/app/src/main/java/com/example/alexanderlombardo/project2buggy/MainActivity.java
+++ Buggy/app/src/main/java/com/example/alexanderlombardo/project2buggy/MainActivity.java
@@ -61,30 +61,6 @@ public class MainActivity extends AppCompatActivity {
         //  allows list to be populated by data passed through the custom CursorAdapter.
         mainList.setAdapter(bugCursorAdapter);
         //Seeds the database using the insertBugData Method in the BugSQLiteOpenHelper Class.
-        dbSetup.insertBugData(1, "Honey Bee",
-                "Apis mellifera", "6", "1", "black, yellow",
-                "99% of bees you've seen are hard working ladies.");
-        dbSetup.insertBugData(2, "Praying Mantis",
-                "Stagmomantis californica", "6", "1", "yellow, green, brown",
-                "Also a style of kung-fu");
-        dbSetup.insertBugData(3, "Black Widow",
-                "Latrodectus hesperus", "8", "0", "black, red",
-                "The western black widow spider or western widow, is a venomous spider species found in western regions of North America.");
-        dbSetup.insertBugData(4, "House Cricket",
-                "Acheta domestica", "6", "1", "brown, grey",
-                "The house cricket is typically gray or brownish in color, growing to 16–21 millimetres (0.63–0.83 in) in length.");
-        dbSetup.insertBugData(5, "Tiger Swallowtail",
-                "Papilio glaucus", "6", "1", "yellow, black",
-                "a species of swallowtail butterfly native to eastern North America");
-        dbSetup.insertBugData(6, "Earthworm",
-                "Lumbricus terrestris", "0", "0", "brown, pink",
-                "An earthworm is a tube-shaped, segmented worm found in the phylum Annelida. Earthworms are commonly found living in soil, feeding on live and dead organic matter.");
-        dbSetup.insertBugData(7, "Black Bean Aphid",
-                "Aphis fabae", "6", "1", "black",
-                "Other common names include blackfly, bean aphid and beet leaf aphid. In the warmer months of the year it is found in large numbers on the undersides of leaves and on the growing tips of host plants, including various agricultural crops and many wild and ornamental plants.");
-        dbSetup.insertBugData(8, "Green Aphid",
-                "Acyrthosiphon pisum", "6", "1", "green, yellow",
-                "Acyrthosiphon pisum, commonly known as the pea aphid (and colloquially known as the green dolphin, pea louse, and clover louse), is a sap-sucking insect in the Aphididae family.");
 
         //Dumps a log of the current data held in the mainCursor to the log, does nothing to affect the app while running.
         DatabaseUtils.dumpCursor(mainCursor);
